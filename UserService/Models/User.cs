1fb261c37031302daf7593a98b410f68aec11491
shantosiddiq <shanto.siddiq1@gmail.com>
2025-06-05 04:27:31 +0600

    feat: implement User entity, DbContext, EF Core migration and repository/service structure
    - Added User model with basic properties (Id, Username, Email, Role, CreatedAt)
    - Configured UserDbContext for EF Core with SQL Server
    - Created initial EF Core migration (InitialCreate)
    - Structured folders: Models, Data, Repositories, Services

---
+++ UserService/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }         // Should match Keycloak's UUID
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+
 namespace UserService
 {
     public class Program
@@ -8,6 +11,8 @@ namespace UserService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<UserDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
